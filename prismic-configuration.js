89101793ec3561481fbf53c18e413ff95142aa56
IM Coder <iamacoderguy@gmail.com>
2021-07-14 17:24:28 +0700

    Clone from prismic sample

---
+++ prismic-configuration.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // The repo name for your Prismic repository (found in the url of your repo: https://imcoder0-sample-blog.prismic.io/)
+  prismicRepo: 'imcoder0-sample-blog',
+
+  // Release ID to build (requires access)
+  releaseID: '',
+
+  // access token to access the release
+  accessToken: '',
+}
